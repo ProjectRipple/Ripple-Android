9d58ca5c565c986eb50c24fe493564d1176b9017
James West <jwest.126@gmail.com>
2014-07-15 12:16:27 -0400

    Add range check for getting touch color and ad alpha channel to closeMatch

--- RippleAndroid/src/main/java/com/discoverylab/ripple/android/fragment/ScenarioNoteFragment.java
+++ RippleAndroid/src/main/java/com/discoverylab/ripple/android/fragment/ScenarioNoteFragment.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -23,6 +22,7 @@ import com.discoverylab.ripple.android.R;
  */
 public class ScenarioNoteFragment extends Fragment implements View.OnTouchListener {
 
+    private static final String TAG = ScenarioNoteFragment.class.getSimpleName();
     private static final int COLOR_TOLERANCE = 25;
     private static final int COLOR_FRONT_HEAD = Color.argb(255, 255, 255, 0);
     private static final int COLOR_FRONT_TORSO = Color.argb(255, 0, 255, 0);
@@ -74,14 +74,20 @@ public class ScenarioNoteFragment extends Fragment implements View.OnTouchListen
         final int evX = (int) event.getX();
         final int evY = (int) event.getY();
 
-        Log.d("lkahtse", "ACTION: " + action + " at (x,y) " + evX + ", " + evY);
+        Log.d(TAG, "ACTION: " + action + " at (x,y) " + evX + ", " + evY);
 
         switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                break;
             case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_DOWN:
+                // get color of point user touched
                 int touchColor = getTouchColor(R.id.patient_tag_painted, evX, evY);
 
+                if (touchColor == -1) {
+                    // invalid color
+                    break;
+                }
+                // Check with closeMatch as display colors may not be exactly those specified due to scaling
                 if (closeMatch(COLOR_FRONT_HEAD, touchColor, COLOR_TOLERANCE)) {
 
                     Toast.makeText(getActivity(), "Front head touched.", Toast.LENGTH_SHORT).show();
@@ -111,23 +117,51 @@ public class ScenarioNoteFragment extends Fragment implements View.OnTouchListen
         return true;
     }
 
-    private int getTouchColor(int paintedImageId, int evX, int evY) {
-        ImageView img = (ImageView) getView().findViewById(paintedImageId);
+    /**
+     * Return the color of the pixel at coordinates (x,y) of the specified ImageView
+     *
+     * @param paintedImageId Resource id of image view
+     * @param x              X coordinate of the pixel to retrieve
+     * @param y              Y coordinate of the pixel to retrieve
+     * @return Color of the pixel at (x,y) of the Imageview or -1 if point is outside the image area
+     */
+    private int getTouchColor(int paintedImageId, int x, int y) {
+        int rtnValue = -1;
+        if (x > 0 && y > 0) {
 
-        img.setDrawingCacheEnabled(true);
-        Bitmap hotspots = Bitmap.createBitmap(img.getDrawingCache());
-        img.setDrawingCacheEnabled(false);
-        return hotspots.getPixel(evX, evY);
+            ImageView img = (ImageView) getView().findViewById(paintedImageId);
 
+            if (img != null) {
+                img.setDrawingCacheEnabled(true);
+                Bitmap hotspots = Bitmap.createBitmap(img.getDrawingCache());
+                img.setDrawingCacheEnabled(false);
+                if (x < hotspots.getWidth() && y < hotspots.getHeight()) {
+                    rtnValue = hotspots.getPixel(x, y);
+                }
+            }
+        }
+        return rtnValue;
     }
 
+    /**
+     * Check if color1 and color2 are a close match (difference within the tolerance per channel)
+     *
+     * @param color1    An ARGB color value
+     * @param color2    ARGB color to compare with the first
+     * @param tolerance Allowable difference between the colors for each color channel
+     * @return true if colors are within the tolerance of each other, false otherwise
+     */
     private boolean closeMatch(int color1, int color2, int tolerance) {
-        if ((int) Math.abs(Color.red(color1) - Color.red(color2)) > tolerance)
-            return false;
-        if ((int) Math.abs(Color.green(color1) - Color.green(color2)) > tolerance)
-            return false;
-        if ((int) Math.abs(Color.blue(color1) - Color.blue(color2)) > tolerance)
-            return false;
-        return true;
+        boolean rtnValue = true;
+        if (Math.abs(Color.alpha(color1) - Color.alpha(color2)) > tolerance) {
+            rtnValue = false;
+        } else if (Math.abs(Color.red(color1) - Color.red(color2)) > tolerance) {
+            rtnValue = false;
+        } else if (Math.abs(Color.green(color1) - Color.green(color2)) > tolerance) {
+            rtnValue = false;
+        } else if (Math.abs(Color.blue(color1) - Color.blue(color2)) > tolerance) {
+            rtnValue = false;
+        }
+        return rtnValue;
     }
 }
