d91975dbd1af01760308f58816435e959d4108b6
James West <jwest.126@gmail.com>
2014-07-17 17:13:57 -0400

    Added border to triage color spinner views

--- RippleAndroid/src/main/java/com/discoverylab/ripple/android/adapter/ui/ColorSpinnerAdapter.java
+++ RippleAndroid/src/main/java/com/discoverylab/ripple/android/adapter/ui/ColorSpinnerAdapter.java
@@ -1,10 +1,16 @@
 package com.discoverylab.ripple.android.adapter.ui;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import com.discoverylab.ripple.android.R;
 
 import java.util.List;
 
@@ -21,16 +27,37 @@ public class ColorSpinnerAdapter extends ArrayAdapter<Integer> implements Spinne
     @Override
     public View getDropDownView(int position, View convertView, ViewGroup parent) {
         View v = super.getDropDownView(position, convertView, parent);
-        // set background
-        v.setBackgroundColor(getItem(position));
+
+        // Set custom background
+        v.setBackgroundResource(R.drawable.color_back_with_border);
+
+        // set center color of view
+        LayerDrawable background = (LayerDrawable) v.getBackground();
+        ((GradientDrawable)background.getDrawable(1)).setColor(getItem(position));
+
+        // hide text by matching background & set to nothing
+        ((TextView)v).setTextColor(getItem(position));
+        ((TextView)v).setText("");
+
+
         return v;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View v = super.getView(position, convertView, parent);
-        // set background
-        v.setBackgroundColor(getItem(position));
+
+        // set custom background
+        v.setBackgroundResource(R.drawable.color_back_with_border);
+
+        // set center color of view
+        LayerDrawable background = (LayerDrawable) v.getBackground();
+        ((GradientDrawable)background.getDrawable(1)).setColor(getItem(position));
+
+
+        // hide text by matching background & set to nothing
+        ((TextView)v).setTextColor(getItem(position));
+        ((TextView)v).setText("");
         return v;
     }
 
